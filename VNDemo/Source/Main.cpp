239bdedab9f1a5f2ed0e714424bb4343933424ca
Tri Nguyen <tri.t.nguyen02@gmail.com>
2019-12-26 00:18:06 -0600

    Created demo project and main engine now compiles into static library

---
+++ VNDemo/Source/Main.cpp
@@ -0,0 +1,51 @@
+#include <vld.h>
+
+#include <Engine/Engine.h>
+#include <Engine/Resource.h>
+
+#include <iostream>
+
+using namespace vne;
+
+// ============================================================================
+
+Uint8 gResourceKey[] =
+{
+	0x37,
+	0x77,
+	0x21,
+	0x7A,
+	0x24,
+	0x43,
+	0x26,
+	0x46,
+	0x29,
+	0x4A,
+	0x40,
+	0x4E,
+	0x63,
+	0x52,
+	0x66,
+	0x55
+};
+
+// ============================================================================
+
+int main()
+{
+	ResourceFolder::setKey(gResourceKey);
+	ResourceFolder::setPath("TestAssets");
+
+	EngineParams params;
+	params.mWindowWidth = 1280;
+	params.mWindowHeight = 720;
+	params.mWindowTitle = "VN Demo";
+	params.mFullscreen = false;
+
+	Engine engine;
+	bool success = engine.init(params);
+	if (success)
+		engine.run();
+
+	return 0;
+}
